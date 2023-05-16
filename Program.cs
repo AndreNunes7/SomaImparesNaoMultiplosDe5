4e24a2c6228ba34de226129e6843e6f3c618f73d
Andre Nunes <92069471+AndreNunes7@users.noreply.github.com>
2023-05-16 01:09:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SomaImparesNaoMultiploDe5
+{
+    public class SomaImparesNaoMultiploDe5
+    {
+        public static int Main(String[] args)
+        {
+            int soma1 = somaImparesNaoMultiplosDe5();
+            Console.WriteLine("Soma dos numeros não multiplos de 5 até 1000 é: " + soma1);
+            
+
+            return 0;
+        }
+
+        public static int somaImparesNaoMultiplosDe5()
+
+        {
+            int soma = 0;
+
+            for (int i = 100; i <= 1000; i++)
+
+            {
+                if (i % 2 != 0 && i % 5 != 0)
+                {
+
+                    soma += i;
+
+                }
+
+
+            }
+            return soma;
+        }
+
+    }
+
+}
